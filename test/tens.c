c218a803d0437bccb7d140e1cedc2637e735663f
Edoardo Giaimo <eg1918@ic.ac.uk>
2020-06-20 20:33:30 +0200

    3d tensor test

--- test/tens.c
+++ test/tens.c
@@ -6,14 +6,19 @@ MU_TEST(test_tens_get)
     float arr[] = {
         1, 2,
         3, 4,
-        5, 6
+        5, 6,
+
+        7, 8,
+        9, 10,
+        11, 12
     };
-    struct tens A = tens(arr, 2, (size_t[]) {3,2});
+    struct tens A = tens(arr, 3, (size_t[]) {2, 3, 2});
 
-    mu_assert_int_eq(3, A.shape[0]);
-    mu_assert_int_eq(2, A.shape[1]);
+    mu_assert_int_eq(2, A.shape[0]);
+    mu_assert_int_eq(3, A.shape[1]);
+    mu_assert_int_eq(2, A.shape[2]);
 
-    mu_assert_float_eq(6, tens_get(A, (size_t[]) {2, 1}));
+    mu_assert_float_eq(12, tens_get(A, (size_t[]) {1, 2, 1}));
 }
 
 MU_TEST_SUITE(test_tens)
