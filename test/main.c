2667114e37347da8dbc11ad5cb609748012df1ef
Edoardo Giaimo <eg1918@ic.ac.uk>
2020-06-23 17:40:59 +0200

    don't need stuct mat anymore

--- test/main.c
+++ test/main.c
@@ -1,11 +1,9 @@
 #include "minunit.h"
-#include "mat.c"
 #include "tensor.c"
 #include "tensindex.c"
 
 int main()
 {
-    MU_RUN_SUITE(test_mat);
     MU_RUN_SUITE(test_tensor);
     MU_RUN_SUITE(test_tensindex);
 }
