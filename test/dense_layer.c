ba15f622cbfcce3c8711fb369c5a251227ffb309
Edoardo Giaimo <eg1918@ic.ac.uk>
2020-07-08 16:17:28 +0200

    testing dense_layer batch shapes

--- test/dense_layer.c
+++ test/dense_layer.c
@@ -7,9 +7,22 @@ MU_TEST(test_dense_feedforward_single)
     struct tensor data_in = tens_range(0, 15, 1);
     data_in = tens_addaxes(&data_in, 0, 1);
     layer->feedforward(layer, &data_in);
+    mu_check(tens_match((struct tens_shape) {2, {1, 4}}, 
+                layer->data_out.shape));
+}
+
+MU_TEST(test_dense_feedforward_batch)
+{
+    struct nn_layer *layer = dense_layer(4);
+    struct tensor data_in = tens_range(0, 45, 1);
+    data_in = tens_reshape(&data_in, (struct tens_shape) {2, {3, 15}});
+    layer->feedforward(layer, &data_in);
+    mu_check(tens_match((struct tens_shape) {2, {3, 4}}, 
+                layer->data_out.shape));
 }
 
 MU_TEST_SUITE(test_dense_layer)
 {
     MU_RUN_TEST(test_dense_feedforward_single);
+    MU_RUN_TEST(test_dense_feedforward_batch);
 }
